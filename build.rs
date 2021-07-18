d027a098380c2513566a7d0b072a0255aeb4e439
Emerentius <emerentius@arcor.de>
2021-07-18 15:45:09 +0200

    fix build on windows
    because of course there are python differences again

--- build.rs
+++ build.rs
@@ -1,7 +1,13 @@
+use std::process::Command;
+
 fn main() {
     println!("cargo:rerun-if-changed=pip_shim/pip");
-    std::process::Command::new("python3")
-        .arg("pip_shim/gen_zip_archive.py")
-        .status()
-        .unwrap();
+    let mut command = if cfg!(windows) {
+        let mut cmd = Command::new("py");
+        cmd.arg("-3");
+        cmd
+    } else {
+        Command::new("python3")
+    };
+    command.arg("pip_shim/gen_zip_archive.py").status().unwrap();
 }
